f6393bda4b94f97bd6a63a3213a3e0d3c03174ed
Joy O'Halloran <johalloran@protonmail.com>
2024-10-01 15:00:41 -0700

    added session store with login, logout, signup thunks

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,21 +1,22 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
-  // ADD REDUCERS HERE
+  session: sessionReducer,
 });
 
 let enhancer;
-if (import.meta.env.MODE === "production") {
+if (import.meta.env.MODE === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = (await import("redux-logger")).default;
+  const logger = (await import('redux-logger')).default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const configureStore = (preloadedState) => {
+const configureStore = preloadedState => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
 
