0d79428c0e84473b4b27bb73d6c21fbc9a5b656d
Joy O'Halloran <johalloran@protonmail.com>
2024-10-01 15:27:16 -0700

    login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <h1> Hello from App </h1>;
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginForm from "./components/SessionForms/LoginFormPage";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Landing page</h1>
+  },
+  {
+    path: '/login',
+    element: <LoginForm />
+  }
+])
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
