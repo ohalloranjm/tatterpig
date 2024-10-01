32c240fecedbe9d9e1ef4a9c5b97b0ba394332d8
Joy O'Halloran <johalloran@protonmail.com>
2024-10-01 13:03:42 -0700

    auth utils, restoreUser middleware added

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,8 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
 
-router.get('/csrf/restore', (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie('XSRF-TOKEN', csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken,
-  });
-});
-
-router.post('/test', function (req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.json({ requestBody: req.body });
-});
+// if current user session is valid, set req.user to that user in the database
+// if not, set req.user to null
+router.use(restoreUser);
 
 module.exports = router;
