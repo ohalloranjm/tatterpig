3fb22d4c3a4839ed0b26caaed59a3e6b138a1621
Joy O'Halloran <johalloran@protonmail.com>
2024-10-01 11:59:30 -0700

    bug: csrf-restore not working

---
+++ backend/routes/api/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+router.get('/csrf/restore', (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie('XSRF-TOKEN', csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken,
+  });
+});
+
+router.post('/test', function (req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
