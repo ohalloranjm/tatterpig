041899212c4eb50eb3815f883b84575e1d574e1e
Joy O'Halloran <johalloran@protonmail.com>
2024-10-01 13:33:27 -0700

    logout route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,4 +39,10 @@ router.post('/', async (req, res, next) => {
   });
 });
 
+// log out a user
+router.delete('/', (_req, res) => {
+  res.clearCookie('token');
+  return res.json({ message: 'success' });
+});
+
 module.exports = router;
