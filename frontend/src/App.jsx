7944b12aafa763d3eda58041e4c2a63cd417c2d8
Joy O'Halloran <johalloran@protonmail.com>
2024-10-01 15:35:33 -0700

    restore session user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,39 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import LoginForm from "./components/SessionForms/LoginFormPage";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { restoreUser } from "./store/session";
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <div>
+      {isLoaded && <Outlet />}
+    </div>
+  );
+}
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <h1>Landing page</h1>
-  },
-  {
-    path: '/login',
-    element: <LoginForm />
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Landing page</h1>
+      },
+      {
+        path: '/login',
+        element: <LoginForm />
+      }
+    ]
   }
 ])
 
