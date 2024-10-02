4f05f6e78c1ea9ce1bb0940a17eee9e023cdf972
Joy O'Halloran <johalloran@protonmail.com>
2024-10-01 19:55:39 -0700

    added sheets table

---
+++ backend/database/models/sheet.js
@@ -0,0 +1,35 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Sheet extends Model {
+    static associate(models) {
+      Sheet.belongsTo(models.User, { foreignKey: 'ownerId' });
+    }
+  }
+  Sheet.init(
+    {
+      ownerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      public: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      description: {
+        type: DataTypes.STRING(2000),
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Sheet',
+    }
+  );
+  return Sheet;
+};
