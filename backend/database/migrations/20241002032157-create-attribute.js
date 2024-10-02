052317a347f4feffca59b2aec4d166c142b2cde9
Joy O'Halloran <johalloran@protonmail.com>
2024-10-02 06:15:47 -0700

    added options variable to create-attribute and create-sheetattribute up  migrations

--- backend/database/migrations/20241002032157-create-attribute.js
+++ backend/database/migrations/20241002032157-create-attribute.js
@@ -7,45 +7,49 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Attributes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      ownerId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users',
+    await queryInterface.createTable(
+      'Attributes',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
+        ownerId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Users',
+          },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        },
+        name: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+        },
+        ephemeral: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+        dataType: {
+          type: Sequelize.STRING(10),
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
       },
-      name: {
-        type: Sequelize.STRING(50),
-        allowNull: false,
-      },
-      ephemeral: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-      },
-      dataType: {
-        type: Sequelize.STRING(10),
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Attributes';
--- backend/database/migrations/20241002034627-create-sheet-attribute.js
+++ backend/database/migrations/20241002034627-create-sheet-attribute.js
@@ -7,43 +7,47 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('SheetAttributes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      sheetId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Sheets',
+    await queryInterface.createTable(
+      'SheetAttributes',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
-      },
-      attributeId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Attributes',
+        sheetId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Sheets',
+          },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
         },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
+        attributeId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Attributes',
+          },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        },
+        value: {
+          type: Sequelize.STRING(500),
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      value: {
-        type: Sequelize.STRING(500),
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'SheetAttributes';
