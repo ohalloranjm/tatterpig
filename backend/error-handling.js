a25e85623422bc4b270ee512ccf9cd3c677342e6
Joy O'Halloran <johalloran@protonmail.com>
2024-10-02 07:16:19 -0700

    moved error handling to separate router

---
+++ backend/error-handling.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { ValidationError } = require('sequelize');
+
+const router = express.Router();
+
+// catch unhandled requests and forward to error handler
+router.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = 'Resource Not Found';
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// catch sequelize errors
+router.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// error format
+router.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    messsage: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+module.exports = router;
