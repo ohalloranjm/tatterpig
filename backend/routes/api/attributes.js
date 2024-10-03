733a919086294c3b92faf094f2e228b60c93648a
Joy O'Halloran <johalloran@protonmail.com>
2024-10-03 07:19:59 -0700

    added GET /api/attributes/:attributeId

--- backend/routes/api/attributes.js
+++ backend/routes/api/attributes.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { Attribute } = require('../../database/models');
+const { Attribute, Sheet, SheetAttribute } = require('../../database/models');
 const { requireAuth } = require('../../utils/auth');
 const { AuthorizationError, NotFoundError } = require('../../utils/errors');
+const {
+  formatAttributeSheetsMutate,
+} = require('../../utils/response-formatting');
 
 const router = express.Router();
 
@@ -10,6 +13,22 @@ router.get('/current', requireAuth, async (req, res) => {
   const { id: ownerId } = req.user;
   const attributes = await Attribute.findAll({ where: { ownerId } });
   return res.json({ attributes });
+});
+
+// view the details of a specific attribute
+router.get('/:attributeId', requireAuth, async (req, res) => {
+  console.log('hitting it');
+  const { attributeId } = req.params;
+  const attribute = await Attribute.findByPk(attributeId, {
+    include: { model: SheetAttribute, include: Sheet },
+  });
+
+  if (!attribute) throw new NotFoundError('Attribute not found');
+  if (attribute.ownerId !== req.user.id) throw new AuthorizationError();
+
+  formatAttributeSheetsMutate(attribute.SheetAttributes);
+
+  return res.json({ attribute });
 });
 
 module.exports = router;
