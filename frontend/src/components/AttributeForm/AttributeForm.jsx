05d516fdf11830c292fcaa6d0761262776e7966a
Joy O'Halloran <johalloran@protonmail.com>
2024-10-03 08:39:34 -0700

    added form with controlled inputs

--- frontend/src/components/AttributeForm/AttributeForm.jsx
+++ frontend/src/components/AttributeForm/AttributeForm.jsx
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+
 export default function AttributeForm() {
-  return <h1>Create a New Attribute</h1>;
+  const [name, setName] = useState('');
+  const [type, setType] = useState('number');
+
+  const post = e => {
+    e.preventDefault();
+    console.log(name, type);
+  };
+
+  return (
+    <>
+      <h1>Create a New Attribute</h1>
+      <form onSubmit={post}>
+        <input
+          placeholder='Attribute Name'
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <select value={type} onChange={e => setType(e.target.value)}>
+          <option value='number'>Number</option>
+          <option value='string'>String</option>
+          <option value='boolean'>Boolean</option>
+        </select>
+        <button type='submit'>Create Attribute</button>
+      </form>
+    </>
+  );
 }
