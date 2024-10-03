49521e11922ac3802ca328db1fc16cd64d987f73
Joy O'Halloran <johalloran@protonmail.com>
2024-10-03 07:30:17 -0700

    added nav links

--- frontend/src/components/AttributesIndex/AttributeTile.jsx
+++ frontend/src/components/AttributesIndex/AttributeTile.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function AttributeTile({ attribute }) {
   return (
     <div>
       <p>
-        {attribute.name} ({attribute.dataType})
+        <Link to={String(attribute.id)}>
+          {attribute.name} ({attribute.dataType})
+        </Link>
       </p>
     </div>
   );
