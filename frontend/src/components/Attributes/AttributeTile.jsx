f0962e07bf6077ed56d998f38acc2eb37d0c57b4
Joy O'Halloran <johalloran@protonmail.com>
2024-10-05 12:00:57 -0700

    displaying details in right column

--- frontend/src/components/Attributes/AttributeTile.jsx
+++ frontend/src/components/Attributes/AttributeTile.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
 import AttributeDetailsPage from './AttributeDetails';
+import { useAttributeSelection } from './context';
 
 export default function AttributeTile({ attribute }) {
-  const [showDetails, setShowDetails] = useState(false);
-  return (
-    <div onClick={() => setShowDetails(prev => !prev)}>
-      {!showDetails && <p>{attribute.name}</p>}
-      {showDetails && <AttributeDetailsPage attribute={attribute} />}
-    </div>
-  );
+  const { display } = useAttributeSelection();
+  const showDetails = display(<AttributeDetailsPage attribute={attribute} />);
+
+  return <p onClick={showDetails}>{attribute.name}</p>;
 }
