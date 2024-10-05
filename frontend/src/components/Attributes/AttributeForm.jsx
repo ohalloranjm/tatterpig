3615d98c5f32b3b13b2ef099a3134b89de49029f
Joy O'Halloran <johalloran@protonmail.com>
2024-10-04 17:26:37 -0700

    CREATE A NEW ATTRIBUTE IS BACK ONLINE

--- frontend/src/components/Attributes/AttributeForm.jsx
+++ frontend/src/components/Attributes/AttributeForm.jsx
@@ -1,27 +1,19 @@
 import { useEffect, useState } from 'react';
 import { useActionData, useSubmit } from 'react-router-dom';
-import { useModal } from '../../context/Modal';
 
 export default function AttributeForm() {
   const [name, setName] = useState('');
   const [dataType, setDataType] = useState('number');
 
   const submit = useSubmit();
-  const { closeModal } = useModal();
-
-  const { errors } = useActionData() ?? {};
+  const data = useActionData();
   const [submitted, setSubmitted] = useState(false);
-
-  // useEffect(() => {
-  //   if (submitted && !errors) {
-  //     // closeModal();
-  //   }
-  // });
+  const errors = submitted ? data?.errors : {};
 
   const post = e => {
     e.preventDefault();
     submit({ name, dataType }, { method: 'POST', encType: 'application/json' });
-    // setSubmitted(true);
+    setSubmitted(true);
   };
 
   return (
--- frontend/src/components/Attributes/index.jsx
+++ frontend/src/components/Attributes/index.jsx
@@ -1,22 +1,21 @@
 import { useLoaderData } from 'react-router-dom';
 import AttributeTile from './AttributeTile';
 import AttributeForm from './AttributeForm';
-import { useState } from 'react';
-import OpenModalButton from '../OpenModalButton';
+import { useEffect, useState } from 'react';
 
 export default function Attributes() {
   const { attributes } = useLoaderData();
   const [create, setCreate] = useState(false);
 
+  useEffect(() => {
+    setCreate(false);
+  }, [attributes.length]);
+
   return (
     <>
       <h1>My Attributes</h1>
-      {
-        <OpenModalButton
-          modalComponent={<AttributeForm />}
-          buttonText={'clickAButton'}
-        />
-      }
+      <button onClick={() => setCreate(prev => !prev)}>Make a New One</button>
+      {create && <AttributeForm />}
       {attributes.map(a => (
         <AttributeTile key={a.id} attribute={a} />
       ))}
