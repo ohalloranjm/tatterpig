37353cd715ceac28df22d9fc2f7fe828d6b6805e
Joy O'Halloran <johalloran@protonmail.com>
2024-10-07 13:34:06 -0700

    more sheets styling

--- frontend/src/components/SheetsPage/SheetTile.jsx
+++ frontend/src/components/SheetsPage/SheetTile.jsx
@@ -1,10 +1,18 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 export default function SheetTile({ sheet }) {
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const className = `sheets-list-tile${
+    Number(searchParams.get('id')) === sheet.id
+      ? ' sheets-list-tile-active'
+      : ''
+  }`;
+
   return (
     <div
-      className='sheets-list-tile'
+      className={className}
       onClick={() => navigate(`/sheets?id=${sheet.id}`)}
     >
       {sheet.name}
