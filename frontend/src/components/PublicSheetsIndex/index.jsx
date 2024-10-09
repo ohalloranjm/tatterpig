18fd7c6b57cf033a69e263b9f8ca3f1c8d40d305
Joy O'Halloran <johalloran@protonmail.com>
2024-10-09 14:43:44 -0700

    started browse styling

--- frontend/src/components/PublicSheetsIndex/index.jsx
+++ frontend/src/components/PublicSheetsIndex/index.jsx
@@ -1,14 +1,14 @@
 import { useLoaderData } from 'react-router-dom';
 import SheetTile from './SheetTile';
+import './PublicSheetsIndex.css';
 
 export default function PublicSheetsIndex() {
   const { sheets } = useLoaderData();
   return (
-    <>
-      <h1>Public Sheets</h1>
+    <div className='browse-sheets'>
       {sheets.map(s => (
         <SheetTile key={s.id} sheet={s} />
       ))}
-    </>
+    </div>
   );
 }
