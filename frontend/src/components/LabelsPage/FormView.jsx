1223027d0b62f654c3b418b0d9e1af8c9d945f00
Joy O'Halloran <johalloran@protonmail.com>
2024-10-09 11:43:51 -0700

    finished create-label-form styling, minus autofocus

--- frontend/src/components/LabelsPage/FormView.jsx
+++ frontend/src/components/LabelsPage/FormView.jsx
@@ -1,80 +1,46 @@
-import { useEffect, useState } from 'react';
-import { useActionData, useSearchParams, useSubmit } from 'react-router-dom';
+import { useState } from 'react';
+import { useActionData, useSubmit } from 'react-router-dom';
+import dictionary from '../SheetsPage/dictionary';
 
-export default function LabelFormView({ label }) {
+export default function LabelFormView() {
   const [name, setName] = useState('');
   const [dataType, setDataType] = useState('number');
 
   const submit = useSubmit();
   const data = useActionData();
   const [submitted, setSubmitted] = useState(false);
-  const [searchParams] = useSearchParams();
-
-  useEffect(() => {
-    const paramsName = searchParams.has('name');
-    const paramsDataType = searchParams.has('datatype');
-    if (paramsName || paramsDataType) {
-      if (paramsName) setName(searchParams.get('name'));
-      if (paramsDataType) setDataType(searchParams.get('datatype'));
-    } else if (label) {
-      setName(label.name);
-      setDataType(label.dataType);
-    } else {
-      setName('');
-      setDataType('number');
-    }
-  }, [label, searchParams]);
 
   const errors = submitted ? data?.errors : {};
 
-  const post = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     submit({ name, dataType }, { method: 'POST', encType: 'application/json' });
     setSubmitted(true);
   };
 
-  const put = e => {
-    e.preventDefault();
-
-    if (dataType !== label.dataType) {
-      const confirmChange = window.confirm(
-        "Are you sure you want to change the data type of this label? Doing so will reset the label's value on every associated sheet."
-      );
-      if (!confirmChange) return setDataType(label.dataType);
-    }
-    submit(
-      { name, dataType },
-      {
-        method: 'PUT',
-        encType: 'application/json',
-        action: `/labels?id=${label.id}&edit=true&name=${name}&datatype=${dataType}`,
-      }
-    );
-    setSubmitted(true);
-  };
-
   return (
-    <div>
-      <h2>{label ? `Editing ${label.name}` : 'Create an Label'}</h2>
+    <form className='create-label-form' onSubmit={handleSubmit}>
+      <h1>Create a Label</h1>
 
-      <form onSubmit={label ? put : post}>
+      <div className='clf-inputs'>
         <input
           placeholder='Label Name'
           value={name}
           onChange={e => setName(e.target.value)}
         />
-        <p className='error'>{submit && errors?.name}</p>
-
         <select value={dataType} onChange={e => setDataType(e.target.value)}>
           <option value='number'>Number</option>
           <option value='string'>String</option>
           <option value='boolean'>Boolean</option>
         </select>
+      </div>
+      <p className='error'>{submit && errors?.name}</p>
 
-        <button type='submit'>
-          {label ? 'Confirm Changes' : 'Create Label'}
-        </button>
-      </form>
-    </div>
+      <div className='clf-dictionary'>{dictionary[dataType]}</div>
+
+      <button type='submit' className='clf-submit'>
+        Create Label
+      </button>
+    </form>
   );
 }
