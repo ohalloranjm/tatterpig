83c23448a725eb9c072208a91da4fbf1405dc36c
Joy O'Halloran <johalloran@protonmail.com>
2024-10-19 08:38:25 -0700

    done with logged-out version of landing page

--- frontend/src/components/Landing/index.jsx
+++ frontend/src/components/Landing/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import LoginForm from '../SessionForms/LoginFormPage';
 import SignupForm from '../SessionForms/SignupFormPage';
 import './Landing.css';
@@ -6,8 +7,19 @@ export default function Landing() {
   return (
     <div className='landing-page'>
       <img className='tatterpiggy' src='/tatterpig.jpg' alt='tatterpig' />
-      <LoginForm />
-      <SignupForm />
+      <div className='landing-page-view'>
+        <p className='about-tatterpig'>
+          <span className='about-tatterpig-name'>Tatterpig</span> is a
+          system-agnostic tabletop roleplaying game (TTRPG) character sheet
+          manager. Create an account to get started, browse{' '}
+          <Link to='/public'>publically available sheets</Link> for inspiration,
+          or use the demo account to sample the site’s features.
+        </p>
+        <div className='session-forms'>
+          <SignupForm />
+          <LoginForm />
+        </div>
+      </div>
     </div>
   );
 }
