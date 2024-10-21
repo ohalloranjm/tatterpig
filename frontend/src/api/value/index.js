237ea49dce4cb92b130bc5c12bed5f80cd1cd05f
Joy O'Halloran <johalloran@protonmail.com>
2024-10-21 16:02:32 -0700

    added putOrder frontend api route

--- frontend/src/api/value/index.js
+++ frontend/src/api/value/index.js
@@ -1,7 +1,8 @@
 import del from './delete';
 import post from './post';
 import put from './put';
+import putOrder from './put-order';
 
-const value = { del, post, put };
+const value = { del, post, put, putOrder };
 
 export default value;
