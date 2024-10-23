abe102cf1349733c6b844fec5d28c213031539a9
Joy O'Halloran <johalloran@protonmail.com>
2024-10-23 06:42:31 -0700

    more public sheet seeds

--- backend/database/seeders/20241002025726-demo-sheet.js
+++ backend/database/seeders/20241002025726-demo-sheet.js
@@ -9,23 +9,31 @@ if (process.env.NODE_ENV === 'production') {
 
 const fakeSheets = [
   {
-    name: 'Dan Bloodaxe',
-    description: "Dan is fightin' man.",
+    name: '5e Template',
+    public: true,
+    description: 'Partial template for D&D 5th edition.',
   },
   {
-    name: 'Space Wizard Template',
+    name: 'Monster of the Week Template',
+    public: true,
+    description: 'Blank character sheet for Monster of the Week.',
+  },
+  {
+    name: 'Lasers and Feelings Template',
+    public: true,
+    description: 'Blank character sheet for the game Lasers and Feelings.',
+  },
+  {
+    name: 'Trick or Treat Template',
     public: true,
     description:
-      'Blank character sheet for the very real game Space Wizards in Space.',
-  },
-  {
-    name: 'Friggilish Fyrewind',
+      'Trick or Treat is a L&F-based one-page RPG about Halloween monsters pretending to be costumed humans.',
   },
 ];
 
 module.exports = {
   async up(_queryInterface, _Sequelize) {
-    const owner = await User.findOne({ where: { username: 'demo' } });
+    const owner = await User.findOne({ where: { username: 'tomorrowind' } });
     const ownerId = owner.id;
     const finalSheets = fakeSheets.map(s => ({ ...s, ownerId }));
     await Sheet.bulkCreate(finalSheets, { validate: true });
@@ -33,7 +41,7 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     options.tableName = 'Sheets';
-    const owner = await User.findOne({ where: { username: 'demo' } });
+    const owner = await User.findOne({ where: { username: 'tomorrowind' } });
     const ownerId = owner.id;
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(
