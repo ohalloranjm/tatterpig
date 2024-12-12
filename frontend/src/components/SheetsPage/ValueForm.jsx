51594938562d0812db9fc8810ed1ea4e69358e0f
Joy O'Halloran <johalloran@protonmail.com>
2024-12-12 14:04:11 -0800

    added comments, refactored to include value inputs for new label

--- frontend/src/components/SheetsPage/ValueForm.jsx
+++ frontend/src/components/SheetsPage/ValueForm.jsx
@@ -2,25 +2,30 @@ import { faArrowLeft, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useRef, useState } from 'react';
 import { useActionData, useLoaderData, useSubmit } from 'react-router-dom';
+import dictionary from './dictionary';
 
 export default function ValueForm({ sheet }) {
-  const { labels } = useLoaderData()[1];
   const submit = useSubmit();
+
+  // extant labels loaded from backend
+  const { labels } = useLoaderData()[1];
+
+  // errors returned by backend
   const { errors } = useActionData() ?? {};
 
-  // controlled label name input
-  const [labelName, setLabelName] = useState('');
-
-  const [selectedLabel, setSelectedLabel] = useState('');
+  // true to create a new label, not just use an existing one
   const [newLabel, setNewLabel] = useState(false);
-  const [newDataType, setNewDataType] = useState(null);
 
+  // controlled inputs
+  const [labelName, setLabelName] = useState('');
+  const [selectedLabel, setSelectedLabel] = useState('');
+  const [newDataType, setNewDataType] = useState(null);
   const [numberValue, setNumberValue] = useState('');
   const [stringValue, setStringValue] = useState('');
 
+  // auto-focus
   const labelInputRef = useRef(null);
   const valueInputRef = useRef(null);
-
   useEffect(() => {
     let toFocus;
     if (selectedLabel && valueInputRef.current) toFocus = valueInputRef;
@@ -36,33 +41,41 @@ export default function ValueForm({ sheet }) {
 
   // identify data type of selected label, if any
   const dataType =
-    newDataType || validChoices.find(vc => vc.id === +selectedLabel)?.dataType;
+    (newLabel && newDataType) ||
+    validChoices.find(vc => vc.id === +selectedLabel)?.dataType;
 
+  // runs when form is submitted
   const handleSubmit = e => {
     e.preventDefault();
 
-    const lookup = {
+    // construct body
+    const value = {
       string: stringValue || null,
       number: numberValue === '' ? null : numberValue,
       boolean: true,
-    };
+    }[dataType];
+    const body = value === null ? {} : { value };
+    if (newLabel) {
+      body.name = labelName;
+    } else {
+      body.labelId = +selectedLabel;
+    }
 
-    const body = {
-      labelId: +selectedLabel,
-      value: lookup[dataType],
-    };
+    // construct frontend api route
+    let action = `/sheets?id=${sheet.id}&add=label`;
+    if (newLabel) action += '&label=new';
 
-    if (body.value === null) delete body.value;
-
+    // submit
     submit(body, {
       method: 'post',
       encType: 'application/json',
-      action: `/sheets?id=${sheet.id}&add=label`,
+      action,
     });
   };
 
   return (
     <form className='sd-add-label' onSubmit={handleSubmit}>
+      {/* display unless an existing label is selected  */}
       {!selectedLabel && (
         <>
           <div className='sdal-name-line'>
@@ -87,17 +100,21 @@ export default function ValueForm({ sheet }) {
               />
             </div>
 
+            {/* New Label button, if clicked opens options for creating a new label */}
             {newLabel ? (
-              <select
-                value={newDataType}
-                onChange={e => setNewDataType(e.target.value)}
-              >
-                {['number', 'string', 'boolean'].map(dt => (
-                  <option key={dt} value={dt}>
-                    {dt}
-                  </option>
-                ))}
-              </select>
+              <>
+                <select
+                  value={newDataType}
+                  onChange={e => setNewDataType(e.target.value)}
+                >
+                  {['number', 'string', 'boolean'].map(dt => (
+                    <option key={dt} value={dt}>
+                      {dt.slice(0, 1).toUpperCase() + dt.slice(1)}
+                    </option>
+                  ))}
+                </select>
+                <div className='sdal-dictionary'>{dictionary[dataType]}</div>
+              </>
             ) : (
               <button
                 type='button'
@@ -110,6 +127,8 @@ export default function ValueForm({ sheet }) {
               </button>
             )}
           </div>
+
+          {/* if New Label is not clicked, display filtered list of labels */}
           {!newLabel && (
             <div className='sdal-name-prompts'>
               {validChoices
@@ -136,6 +155,7 @@ export default function ValueForm({ sheet }) {
         </>
       )}
 
+      {/* special display for confirming an existing label */}
       {!!selectedLabel && (
         <>
           <div className='sdal-locked-name-line'>
@@ -151,6 +171,17 @@ export default function ValueForm({ sheet }) {
             </button>
             <p>{labelName}</p>
           </div>
+          {dataType === 'boolean' && (
+            <p className='sdal-value sdal-value-boolean'>(boolean)</p>
+          )}
+
+          <p className='error sdal-errors'>{errors?.value}</p>
+        </>
+      )}
+
+      {/* value input fields and submit button, show up if existing label or New Label is clicked */}
+      {(!!selectedLabel || newLabel) && (
+        <>
           {dataType === 'number' && (
             <input
               placeholder='Number Value'
@@ -170,19 +201,9 @@ export default function ValueForm({ sheet }) {
               ref={valueInputRef}
             />
           )}
-          {dataType === 'boolean' && (
-            <p className='sdal-value sdal-value-boolean'>(boolean)</p>
-          )}
-
-          <button
-            type='submit'
-            className='icon sdal-submit'
-            disabled={!selectedLabel}
-          >
-            <FontAwesomeIcon icon={faCheck} />
+          <button type='submit' className='sdal-submit'>
+            Submit
           </button>
-
-          <p className='error sdal-errors'>{errors?.value}</p>
         </>
       )}
     </form>
